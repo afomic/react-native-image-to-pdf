1b08a5284cc83c5288d664842e076a572e83046c
michael afolabi <afomic1@gmail.com>
2019-10-28 16:03:01 +0100

    updated dependency

--- ios/RNImageToPdf.h
+++ ios/RNImageToPdf.h
@@ -1,13 +1,13 @@
 //  Created by Jonas
 
 #if __has_include(<React/RCTAssert.h>)
-#import <React/RCTView.h>
+#import  <React/UIView+React.h>
 #import  <React/RCTBridgeModule.h>
 #import  <React/RCTUtils.h>
 #import <React/RCTConvert.h>
 #import <React/RCTEventDispatcher.h>
 #else
-#import "RCTView.h"
+#import "UIView+React.h"
 #import "RCTBridgeModule.h"
 #import "RCTUtils.h"
 #import "RCTConvert.h"
